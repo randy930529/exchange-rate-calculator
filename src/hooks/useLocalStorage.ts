48694ca73efa1967e81780a78cc0814185a89f28
randy930529 <randy930529@gmail.com>
2023-10-02 15:43:59 -0400

    create hook for use local storaje

---
+++ src/hooks/useLocalStorage.ts
@@ -0,0 +1,27 @@
+import { useDebugValue, useEffect, useState } from "react";
+
+export function useLocalStorage(key: string, initialState: any) {
+  const [state, setState] = useState(initialState);
+  useDebugValue(state);
+
+  useEffect(() => {
+    const item = localStorage.getItem(key);
+    if (item) setState(parse(item));
+  }, [key]);
+
+  useEffect(() => {
+    if (state.length > 0) {
+      localStorage.setItem(key, JSON.stringify(state));
+    }
+  }, [state, key]);
+
+  return [state, setState];
+}
+
+function parse(obj: any) {
+  try {
+    return JSON.parse(obj);
+  } catch {
+    return obj;
+  }
+}
