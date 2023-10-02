c207a7f4835c2221413e0977cc958a5d1716b732
randy930529 <randy930529@gmail.com>
2023-10-02 15:40:45 -0400

    set initial file for page home

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,13 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { ExchangeRatesProvider } from "@/context/ExchangeRatesContext";
+import Container from "@/client-components/Container";
+import InputAmount from "@/client-components/InputAmount";
+import { ListOfCurrenciesOption } from "./server-components/ListOfCurrenciesOption";
+import ButtonConvert from "@/client-components/ButtonConvert";
+import { Suspense } from "react";
+import Loading from "@/app/loading";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +23,24 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ExchangeRatesProvider>
+          <Container>
+            <h1>Exchange Rate Calculator</h1>
+            <InputAmount />
+            <ListOfCurrenciesOption
+              key={"fromCurrency"}
+              text={"Select the currency of origin"}
+            />
+            <ListOfCurrenciesOption
+              key={"toCurrency"}
+              text={"Select the destination currency"}
+            />
+            <ButtonConvert />
+            <Suspense fallback={<Loading />}>{children}</Suspense>
+          </Container>
+        </ExchangeRatesProvider>
+      </body>
     </html>
   );
 }
