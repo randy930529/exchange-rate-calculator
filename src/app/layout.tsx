4dc6ceacde74a0f5f3bacabfc2d1911ebc2ea327
Randy <93347169+randy930529@users.noreply.github.com>
2023-10-04 08:14:28 +0000

    get config styles-component in nextjs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import StyledComponentsRegistry from '@/lib/registry'
 import { ExchangeRatesProvider } from "@/context/ExchangeRatesContext";
 import Container from "@/client-components/Container";
 import InputAmount from "@/client-components/InputAmount";
@@ -24,30 +25,34 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ExchangeRatesProvider>
-          <Container>
-            <h1 className="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
-              <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
-                Exchange Rate
-              </span>{" "}
-              Calculator
-            </h1>
+        <StyledComponentsRegistry>
+          <ExchangeRatesProvider>
+            <Container>
+              <div>
+                <h1 className="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
+                  <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
+                    Exchange Rate
+                  </span>{" "}
+                  Calculator
+                </h1>
+              </div>
 
-            <InputAmount />
-            <ListOfCurrenciesOption
-              key={"ListFromCurrency"}
-              name={"fromCurrency"}
-              text={"Select the currency of origin"}
-            />
-            <ListOfCurrenciesOption
-              key={"ListToCurrency"}
-              name={"toCurrency"}
-              text={"Select the destination currency"}
-            />
-            <ButtonConvert />
-            <Suspense fallback={<Loading />}>{children}</Suspense>
-          </Container>
-        </ExchangeRatesProvider>
+              <InputAmount />
+              <ListOfCurrenciesOption
+                key={"ListFromCurrency"}
+                name={"fromCurrency"}
+                text={"Select the currency of origin"}
+              />
+              <ListOfCurrenciesOption
+                key={"ListToCurrency"}
+                name={"toCurrency"}
+                text={"Select the destination currency"}
+              />
+              <ButtonConvert />
+              <Suspense fallback={<Loading />}>{children}</Suspense>
+            </Container>
+          </ExchangeRatesProvider>
+        </StyledComponentsRegistry>
       </body>
     </html>
   );
